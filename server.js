7f276a35f2aa8f518e66838eddee0ae7567bb3f1
RaziyaSultana <raziyasultana1997@gmail.com>
2017-08-04 13:09:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one',funtion(req,res) {
+    res.send('Article one requested and will be  served here.');
+});
+app.get('/article-two',funtion(req,res) {
+    res.send('Article two requested and will be  served here.');
+});
+app.get('/article-three',funtion(req,res) {
+    res.send('Article three requested and will be  served here.');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
