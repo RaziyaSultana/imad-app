1d6a9fba1a4a996ca1ace9a3b0968b358ba5a88a
RaziyaSultana <raziyasultana1997@gmail.com>
2017-08-07 15:31:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,12 @@ console.log('Loaded!');
 //change  the text of main-text div
 var element = document.getElementById('main-text');
 element.innerHTML='New Value';
+
+//move the image
+
+var element = document.getElementById('img');
+
+img.onClick = function(){
+ 
+ img.style.marginLeft='100px';   
+};
