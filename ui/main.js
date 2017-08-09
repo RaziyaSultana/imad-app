0cc73e1ec586f616fb51603d42627d8d9daa47ce
RaziyaSultana <raziyasultana1997@gmail.com>
2017-08-09 20:22:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,8 +30,7 @@ button.onclick=function() {
 
 //submit name
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
@@ -63,6 +62,8 @@ submit.onclick = function() {
   };
   
   //Make the request
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open('GET','http://raziyasultana1997.imad.hasura-app.io/submit-name?name='+name,true);
   request.send(null);
 
