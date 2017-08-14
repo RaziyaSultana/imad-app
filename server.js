f5d77dd3fc0f8bec87758d516a39822354adb1c4
RaziyaSultana <raziyasultana1997@gmail.com>
2017-08-14 17:56:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/test-db', function (req, res) {
           res.status(500).send(err.toString());
       }
       else{
-          res.send(JSON.stringify(result.rows));
+          res.send(result.row);
       }
   })
 });
