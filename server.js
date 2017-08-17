228845a225d05570a6e4246bf2e59874eb71735b
RaziyaSultana <raziyasultana1997@gmail.com>
2017-08-17 20:51:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,14 +61,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash(input, salt){
+function hash(input,salt){
     //How do we cfeate a hash?
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    var hashed = crypto.pbkdf2Sync('input', 'salt', 100000, 512, 'sha512');
+    return (hashed.toString('hex'));
 }
 
 app.get('/hash/:input',function(res, req){
-    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    var hashedString = hash('req.params.input','this-is-some-random-string');
     res.send(hashedString);
     
 });
