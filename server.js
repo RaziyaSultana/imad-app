df2ad00ade2e86f042805b3bec2292bfd81f89c0
RaziyaSultana <raziyasultana1997@gmail.com>
2017-08-17 21:06:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 
 function hash(input,salt){
     //How do we cfeate a hash?
-    var hashed = crypto.pbkdf2Sync('input', 'salt', 100000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
     return hashed.toString('hex');
 }
 
