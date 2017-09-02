6c33f0a0e2a1e74bfb797b10a70ebcdc95287f54
RaziyaSultana <raziyasultana1997@gmail.com>
2017-09-02 22:24:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //submit username/password to login
 
 var submit = document.getElementById('submit_btn');
-submit.onClick = function() {
+submit.onclick = function() {
     
     //Create a request object
   var request= new XMLHttpRequest();
