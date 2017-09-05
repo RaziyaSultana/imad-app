d1009fd0ce482e8da337ac3a307a9f3a6a23b642
RaziyaSultana <raziyasultana1997@gmail.com>
2017-09-05 10:17:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,13 @@
 //submit username/password to login
 
 var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
+submit.onclick = function () {
     
     //Create a request object
   var request= new XMLHttpRequest();
   
   //Capture the response and store it in a variable
-  request.onreadystatechange = function(){
+  request.onreadystatechange = function (){
      if(request.readyState === XMLHttpRequest.DONE){
          //Take some action
          if(request.status === 200){
